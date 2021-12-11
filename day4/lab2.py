11061a09b67c2a1d2705f351becbcf904968a0cf
prasnaaryal <prasna101@gmail.com>
2021-12-11 22:20:48 +0545

    work

---
+++ day4/lab2.py
@@ -0,0 +1,8 @@
+a=int(input("enter a integer"))
+b=int(input("enter a integer"))
+c=int(input("enter a integer"))
+if (a==b==c) or (b==a==c) or (c==a==b):
+     print("the sum is 0")
+else:
+      print("sum:",a+b+c)
+      
